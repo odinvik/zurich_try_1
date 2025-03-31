acdc4dbaa0ba54eace7e740505c46307ca825e2b
Prasenjit Saha <psaha@physik.uzh.ch>
2025-03-31 17:13:30 +0200

    additions

--- downsampler.py
+++ downsampler.py
@@ -8,30 +8,30 @@ def ifft(f):
     return np.fft.fftshift(np.fft.ifft2(np.fft.fftshift(f)))
     #return np.fft.ifft2(f)
 
-fil = plt.imread('galaxy_020.jpg')
 
-fil = plt.imread('bird.jpg')
+def smaller(fil,M):
+    ffil = np.mean(fil/255,2)
+    nx,ny = ffil.shape
+    nx -= nx % 2
+    ny -= ny % 2
+    N = 2
+    while N < nx and N < ny:
+        N *= 2
+    gr = np.zeros((N,N))
+    dx = (N-nx)//2
+    dy = (N-ny)//2
+    gr[dx:nx+dx,dy:ny+dy] = ffil[:nx,:ny]
+    fr = fft(gr)
+    lo = N//2 - M//2
+    hi = N//2 + M//2
+    fr = fr[lo:hi,lo:hi]
+    ngr = ifft(fr).real
+    return ngr,fr
 
-ffil = np.mean(fil/255,2)
-nx,ny = ffil.shape
-nx -= nx % 2
-ny -= ny % 2
-N = 2
-while N < nx and N < ny:
-    N *= 2
-print(N)
-gr = np.zeros((N,N))
-dx = (N-nx)//2
-dy = (N-ny)//2
-gr[dx:nx+dx,dy:ny+dy] = ffil[:nx,:ny]
+img = plt.imread('galaxy_020.jpg')
+img = plt.imread('bird.jpg')
 
-fr = fft(gr)
-M = 512
-lo = N//2 - M//2
-hi = N//2 + M//2
-fr = fr[lo:hi,lo:hi]
-ngr = ifft(fr).real
-
-plt.imshow(ngr,cmap='gray')
-plt.gca().set_aspect(1)
+gray,fou = smaller(img,64)
+#plt.imshow(gray,cmap='gray')
+plt.imshow(abs(fou)**(1/4),cmap='gray')
 plt.show()
