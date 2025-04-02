6927e698dc5ceeae075fe2a18a58788221288982
Prasenjit Saha <psaha@physik.uzh.ch>
2025-04-02 20:10:42 +0200

    edits

--- downsampler.py
+++ downsampler.py
@@ -22,49 +22,29 @@ def insert(a,dx,dy,b):
 
 def smaller(fil,M,ang):
     ffil = np.mean(fil/255,2)
+    # rotate
     ffil = ndimage.rotate(ffil,ang,reshape=True)
-    nx,ny = ffil.shape
-    print(nx,ny)
-    if np.sum(ffil[:nx//2,:]) < np.sum(ffil[nx//2:,:]):
-        ffil = ndimage.rotate(ffil,180,reshape=True)
+    # center the image
     nx,ny = ffil.shape
     dx,dy = ndimage.center_of_mass(ffil)
     dx,dy = dx-nx/2,dy-ny/2
-    print(dx,dy)
+    #print(dx,dy)
     dx,dy = int(abs(dx)),int(abs(dy))
     gray = np.zeros(shape=(nx+2*dx,ny+2*dy))
-    print(gray.shape)
-    print(dx,dy)
-    '''
-    if dx == dy == 0:
-        gray[:,:] = ffil[:,:]
-    elif dx == 0:
-        gray[:,dy:-dy] = ffil[:,:]
-    elif dy == 0:
-        gray[dx:-dx,:] = ffil[:,:]
-    else:
-        gray[dx:-dx,dy:-dy] = ffil[:,:]
-    '''
+    #print(gray.shape)
+    #print(dx,dy)
     insert(gray,dx,dy,ffil)
     nx,ny = gray.shape
     dx,dy = ndimage.center_of_mass(gray)
     dx,dy = dx-nx/2,dy-ny/2
-    print(dx,dy)
     gray = ndimage.shift(gray,(-dx,-dy))
+    # check is centered
     nx,ny = gray.shape
     dx,dy = ndimage.center_of_mass(gray)
     dx,dy = dx-nx/2,dy-ny/2
     print(dx,dy)
-    '''
-    return gray,gray
-    ffil = ndimage.rotate(ffil,ang,reshape=True)
-    cm = ndimage.center_of_mass(ffil)
-    nx,ny = ffil.shape
-    print(nx,ny,cm)
-    if cm[0] < nx/2:
-        ffil = ndimage.rotate(ffil,180,reshape=False)
-    '''
-    print(nx,ny)
+    #print(nx,ny)
+    # made even
     nx -= nx % 2
     ny -= ny % 2
     N = 2
@@ -73,9 +53,13 @@ def smaller(fil,M,ang):
     gr = np.zeros((N,N))
     dx = N//2-nx//2
     dy = N//2-ny//2
-    print(N,nx,ny,dx,dy)
-    #gr[dx:-dx,dy:-dy] = gray[:nx,:ny]
     insert(gr,dx,dy,gray[:nx,:ny])
+    # brighter side above
+    nx,ny = ffil.shape
+    if np.sum(gr[:N//2,:]) < np.sum(gr[N//2:,:]):
+        print('reversing')
+        gr = ndimage.rotate(gr,180)
+    # downsample
     fr = fft(gr)
     lo = N//2 - M//2
     hi = N//2 + M//2
@@ -84,16 +68,22 @@ def smaller(fil,M,ang):
     return ngr,fr
 
 
-M = 32
+M = 64
 for n in range(1,214):
     if n in (61,73,82,104,105,121,123,144,170,186,205,208,211):
         continue
     img = plt.imread('galaxies/galaxy_'+str(n).zfill(3)+'.jpg')
     gray,fou = smaller(img,M,140)
     plt.clf()
+    plt.subplot(1,4,1)
     plt.title(str(n))
-    #plt.imshow(gray,cmap='gray')
+    plt.imshow(gray,cmap='gray')
+    plt.subplot(1,4,2)
     plt.imshow(abs(fou)**(1/4),cmap='gray')
+    plt.subplot(1,4,3)
+    plt.imshow(fou.real,cmap='coolwarm')
+    plt.subplot(1,4,4)
+    plt.imshow(fou.imag,cmap='coolwarm')
     #plt.show()
     plt.pause(0.5)
 
